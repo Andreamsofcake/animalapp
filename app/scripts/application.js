693c6511bd4107581174ea060faa7e064985092d
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-10-17 12:02:27 -0700

    passing idex tests

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -4,3 +4,8 @@ var AnimalApp = Ember.Application.create();
 
 // expose AnimalApp globally
 window.AnimalApp = AnimalApp;
+
+// routes to the templates
+AnimalApp.Router.map(function() {
+  this.route('animals')
+});
--- test/app/test.js
+++ test/app/test.js
@@ -1,6 +1,14 @@
 'use strict';
 
 describe('app', function() {
-  it('has one passing test', function() {});
-  it('will have more tests');
+  it('has an index page', function() {
+    visit('/');
+    expect(currentRouteName()).to.equal('index');
+  });
+  it('will have a home link and a animals link', function(){
+    visit('/')
+    andThen(function(){
+      expect(find('a.home').length).to.equal(1);
+    })
+  });
 });
